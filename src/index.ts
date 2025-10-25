1cec919c4d321b355d5cc267843ba09a692d137d
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-10-25 10:38:50 +0530

    added the dotenv only for development

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { getClickhouseClient, getKafkaClient } from "./initClients";
 import { initKafkaConsumer } from "./consumer";
 import { connectDb } from "./connectDb";
 import dotenv from "dotenv";
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config(); // only load .env locally
+}
 
 async function main() {
     await connectDb();
